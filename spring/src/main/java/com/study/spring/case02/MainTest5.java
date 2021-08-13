7e63d615478356d7e93abebf0c66e350be433e37
Windows7 <Windows7@Windows7-PC>
2021-08-13 19:17:05 +0800

    spring

---
+++ spring/src/main/java/com/study/spring/case02/MainTest5.java
@@ -0,0 +1,15 @@
+package com.study.spring.case02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainTest5 {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext2.xml");
+		Paper p1 = (Paper)ctx.getBean("paper");
+		System.out.println(p1);
+	}
+
+}
